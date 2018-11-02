5c4d89fcf02410f7903c0cfe9891a9b0d51fd2bb
이규원 <trq1.lee@gmail.com>
2018-11-02 16:20:59 +0900

    BoardDao 작성중

--- src/dao/BoardDao.java
+++ src/dao/BoardDao.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+
 import vo.BoardVo;
 
 public class BoardDao {
@@ -27,7 +29,7 @@ public class BoardDao {
             rs = pstm.executeQuery(sqlCount);
 
             if (rs.next()) {
-                total = rs.getInt(1); // select문 count 값
+                total = rs.getInt(1);
             }
         } catch (SQLException e) {
             System.out.println(e);
@@ -63,6 +65,11 @@ public class BoardDao {
         return password;
     }
 
+
+    /**
+     * 게시판 삭제
+     * @param idx
+     */
     public void sqlDelete(int idx) {
         PreparedStatement pstm = null;
         Connection conn = dbconnect.connDb();
@@ -80,82 +87,35 @@ public class BoardDao {
     }
 
     /**
-     * 게시판 정보를 읽어드리기위한 메소드
-     */
-    public void sqlSelect(int idx) {
-        PreparedStatement pstm = null;
-        ResultSet rs = null;
-        Connection conn = dbconnect.connDb();
-        String authorSelect = "";
-        String titleSelect = "";
-        String contentSelect = "";
-        String todateSelect = "";
-        try {
-            String sqlSelect = "SELECT author, title, content, todate FROM board WHERE id=" + idx;
-            pstm = conn.prepareStatement(sqlSelect);
-            rs = pstm.executeQuery(sqlSelect);
-
-            if (rs.next()) {
-                authorSelect = rs.getString(1);
-                titleSelect = rs.getString(2);
-                contentSelect = rs.getString(3);
-                todateSelect = rs.getString(4);
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        } finally {
-            dbconnect.close(pstm, conn);
-            dbconnect.resultClose(rs);
-        }
-        return;
-    }
-
-    /**
-     * sqlList를 리턴하기 위한 class
-     */
-    /**
-     *
-     * @param start
-     * @param end
      * 게시판 글 리스트를 읽어드리기 위한 메소드
      */
-    public void sqlList(int start, int end) {
+    public ArrayList<BoardVo> sqlList() {
         PreparedStatement pstm = null;
         ResultSet rs = null;
         Connection conn = dbconnect.connDb();
-        int id = 0;
-        String author = "";
-        String todate = "";
-        String title = "";
-        String todateBefore = "";
-        String checkTitle = "";
+
+        ArrayList<BoardVo> boardList = new ArrayList<BoardVo>();
         try {
-            String sqlList = "SELECT id, author, title, todate from board where id >=" + start + " and id <= " + end + " order by id DESC";
+
+            String sqlList = "SELECT id, author, title, todate from board order by id DESC";
             pstm = conn.prepareStatement(sqlList);
-            rs = pstm.executeQuery(sqlList);
+            rs = pstm.executeQuery();
 
             while (rs.next()) {
-                id = rs.getInt(1);
-                author = rs.getString(2);
-                checkTitle = rs.getString(3);
-                todateBefore = rs.getString(4);
-                todate = todateBefore.substring(0, todateBefore.length() - 2); // 소수점 자르기 추후 데이터 타입 나오게 할 예정
+                BoardVo boardVo = new BoardVo();
+                boardVo.setId(rs.getInt(1));
+                boardVo.setAuthor(rs.getString(2));
+                boardVo.setTitle(rs.getString(3));
+                boardVo.setTodate(rs.getDate(4));
 
-                // checkTitle 에 200자 이상인 경우 ... 을 붙인다.
-                int titleLen = checkTitle.length();
-
-                if (titleLen > 200) { // 200 이상인 경우 titlelen 숫자에서 - 10 을 한후 뒤에 ... 을 붙인다.
-                    title = checkTitle.substring(0, titleLen - 10) + "...";
-                } else {
-                    title = checkTitle;
-                }
+                boardList.add(boardVo);
             }
         } catch (SQLException e) {
             System.out.println(e);
         } finally {
             dbconnect.close(pstm, conn);
             dbconnect.resultClose(rs);
-        }
+        } return boardList;
     }
 
     /**
@@ -179,11 +139,6 @@ public class BoardDao {
     }
 
     /**
-     *
-     * @param authorInsert
-     * @param passwordInsert
-     * @param titleInsert
-     * @param contentInsert
      *
      * 게시판 글을 쓰기 위한 메소드
      */
