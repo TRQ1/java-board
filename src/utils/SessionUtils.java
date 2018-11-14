50fc810aafe1cf2e470b45f0ad067a084880623f
이규원 <trq1.lee@gmail.com>
2018-11-14 09:29:35 +0900

    SesstionUitls 작성 준비

---
+++ src/utils/SessionUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+import javax.servlet.http.HttpSession;
+import java.io.Serializable;
+import java.util.Date;
+
+public class SessionUtils implements Serializable {
+
+    public void setSession() {
+
+    }
+
+}
